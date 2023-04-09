03a33ffd2f4d97e679b551a8afb34f53a3952338
Yudu Eswar Vinay Pratap Kumar Kuchi <k.y.e.v.prathapkumar33@gmail.com>
2023-04-08 20:06:06 -0500

    Update Question6.java

--- KuchiAssignment03/src/assignment03/Question6.java
+++ KuchiAssignment03/src/assignment03/Question6.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Kuchi ";
+        StringBuffer sb = new StringBuffer("Kuchi");
+
+        
+        str += " Pratap";
+        sb.append(" Kumar");
+
+       
+        System.out.println("String: " + str);
+        System.out.println("StringBuffer: " + sb);
+    }
+}
 
