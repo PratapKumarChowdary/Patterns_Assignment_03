46796b90c239d4d65dc73c898201cabd9e38e4a6
Yudu Eswar Vinay Pratap Kumar Kuchi <k.y.e.v.prathapkumar33@gmail.com>
2023-04-08 21:54:20 -0500

    Create Question22.java

---
+++ KuchiAssignment03/src/assignment03/Question22.java
@@ -0,0 +1,18 @@
+// Here person class is final and two instance variable were also final means immutable
+public final class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
