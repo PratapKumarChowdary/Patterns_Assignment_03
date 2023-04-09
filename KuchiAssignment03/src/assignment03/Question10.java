642fde2c11362b51b555f212ce1a28e95bfdba00
Yudu Eswar Vinay Pratap Kumar Kuchi <k.y.e.v.prathapkumar33@gmail.com>
2023-04-08 20:39:14 -0500

    Create Question10.java

---
+++ KuchiAssignment03/src/assignment03/Question10.java
@@ -0,0 +1,11 @@
+class Superclass {
+    void doSomething() throws IOException {
+        // Sample Code
+    }
+}
+
+class Subclass extends Superclass {
+    void doSomething() throws FileNotFoundException {
+        // Sample Code
+    }
+}
