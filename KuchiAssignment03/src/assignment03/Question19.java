33b2a5296f2ed6385c180826bcfa46e7804fc6ed
Yudu Eswar Vinay Pratap Kumar Kuchi <k.y.e.v.prathapkumar33@gmail.com>
2023-04-08 21:45:51 -0500

    Update Question19.java

--- KuchiAssignment03/src/assignment03/Question19.java
+++ KuchiAssignment03/src/assignment03/Question19.java
@@ -1,1 +1,20 @@
 
+public class MyThread extends Thread {
+    public void run() {
+        System.out.println("Hello from MyThread! using Thread Class");
+    }
+}
+
+MyThread thread = new MyThread();
+thread.start();
+
+
+public class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Hello from MyRunnable Interface!");
+    }
+}
+
+MyRunnable runnable = new MyRunnable();
+Thread thread = new Thread(runnable);
+thread.start();
